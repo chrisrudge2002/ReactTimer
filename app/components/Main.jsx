fd1acb758c6fe6decc8fe7b7b00ec854930494b5
Chris Rudge <chrisrudge2002@gmail.com>
2017-01-31 18:13:53 +0000

    Init Repo

---
+++ app/components/Main.jsx
@@ -0,0 +1,16 @@
+const React = require('react');
+
+const Main = (props) => {
+	return (
+		<div>
+			<div>
+				<div>
+					<p>Main.jsx Rendered</p>
+					{props.children}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+module.exports = Main;
